6de90743df2243a6028ac92dcaae8bd085cbaae3
Christopher Paciorek <paciorek@Christophers-MacBook-Air.local>
2018-05-18 09:45:33 -0400

    more progress on Rnw -> Rmd conversion

--- UserManual/bookdown/latex2Rmd.R
+++ UserManual/bookdown/latex2Rmd.R
@@ -10,36 +10,57 @@
 #' @author Loic Dutrieux
 #' 
 #' @import stringr
-#' 
+#'
+#'
+library(stringr)
 
-latex2Rmd <- function(input, output, save = TRUE) {
-    text <- readChar(input, file.info(input)$size)
+#1) upgrade R on laptop (checkForSelfParents)
+#3) Rnw % comments to what in Rmd?
+#4) Section numbers?
+#5) @ref(sec-foo) not working
+#6) footnotes
+# parts
+# chapter ordering
+                                        # markup thing is not working
+# for \n included with . could do regex("<<.*>>", dotall=TRUE)
+
+latex2Rmd <- function(input, save = TRUE) {
+    inputFile <- paste0(input, ".Rnw")
+    text <- readChar(inputFile, file.info(inputFile)$size)
+     text <- str_replace_all(string = text, pattern = ",results='markup;\\$=\\\\\\$'", '')
+    text <- str_replace_all(string = text, pattern = '<<([\\s\\S]*?)>>=([\\s\\S]*?)@', replacement = '```{r, \\1}\\2```')
     
-    text <- str_replace_all(string = text, pattern = '<<(.*?)>>=(.*?)@', replacement = '```{r, \\1}\\2```')
-    
-    # Order these in likelihood of nestedness
-    text <- str_replace_all(string = text, pattern = '\\\\href\\{(.*?)\\}\\{(.*?)\\}', replacement = '[\\2](\\1)')
-    text <- str_replace_all(string = text, pattern = '\\\\url\\{(.*?)\\}', replacement = '[\\1]')
-    text <- str_replace_all(string = text, pattern = '\\\\code\\{(.*?)\\}', replacement = '`\\1`')
-    text <- str_replace_all(string = text, pattern = '\\\\texttt\\{(.*?)\\}', replacement = '`\\1`')
-    text <- str_replace_all(string = text, pattern = '\\\\(emph|nm)\\{(.*?)\\}', replacement = '*\\2*')
-    text <- str_replace_all(string = text, pattern = '\\\\textit\\{(.*?)\\}', replacement = '*\\1*')
+                                        # Order these in likelihood of nestedness
+    text <- str_replace_all(string = text, pattern = "%.*?\\n", "")
+    text <- str_replace_all(string = text, pattern = '\\\\href\\{([\\s\\S]*?)\\}\\{([\\s\\S]*?)\\}', replacement = '[\\2](\\1)')
+    text <- str_replace_all(string = text, pattern = '\\\\url\\{([\\s\\S]*?)\\}', replacement = '[\\1]')
+    text <- str_replace_all(string = text, pattern = '\\\\(texttt|code|file)\\{(.*?)\\}', replacement = '`\\2`')
+    text <- str_replace_all(string = text, pattern = '\\\\(textit|emph|nm)\\{(.*?)\\}', replacement = '*\\2*')
    
     text <- str_replace_all(string = text, pattern = '\\\\chapter\\*?\\{(.*?)\\}', replacement = '# \\1') 
     text <- str_replace_all(string = text, pattern = '\\\\section\\*?\\{(.*?)\\}', replacement = '## \\1')
     text <- str_replace_all(string = text, pattern = '\\\\subsection\\*?\\{(.*?)\\}', replacement = '### \\1')
     text <- str_replace_all(string = text, pattern = '\\\\subsubsection\\*?\\{(.*?)\\}', replacement = '#### \\1')
     
-    text <- str_replace_all(string = text, pattern = '\\\\label\\*?\\{(.*?)\\}', replacement = '\\{#\\1\\}')
+    text <- str_replace_all(string = text, pattern = '\\n\\\\label\\*?\\{(.*?)\\}', replacement = ' \\{#\\1\\}')
     text <- str_replace_all(string = text, pattern = '\\\\ref\\*?\\{(.*?)\\}', replacement = '@ref\\(\\1\\)')
     
     text <- str_replace_all(string = text, pattern = '\\\\cd\\*?\\{(.*?)\\}', replacement = '`\\1`') 
     text <- str_replace_all(string = text, pattern = '\\\\begin\\{(.*?)\\}', replacement = '')
     text <- str_replace_all(string = text, pattern = '\\\\end\\{(.*?)\\}', replacement = '')
     text <- str_replace_all(string = text, pattern = '\\\\item', replacement = '  *')
-    
-    text <- str_replace_all(string = text, pattern = '\\\r', replacement = '')
 
-    text <- str_replace_all(string = text, pattern = '\\\\citep\\*?\\{(.*?)\\}', replacement = '[@\\1]') 
-    if(save)  writeLines(text, output, sep = '')
+    text <- str_replace_all(string = text, pattern = '\\\\verb\\|(.*?)\\|', replacement = '`\\1`')
+
+    text <- str_replace_all(string = text, pattern = '\\\r', replacement = '')
+    text <- str_replace_all(string = text, pattern = '\\\\ver', replacement = '`r version`')
+
+    text <- str_replace_all(string = text, pattern = '\\\\footnote\\{([\\s\\S]*?)\\}', replacement = ' [\\1]')
+    text <- str_replace_all(string = text, pattern = '\\\\Sexpr\\{(.*?)\\}', replacement = '')
+    text <- str_replace_all(string = text, pattern = "``(.*?)''", "'\\1'")
+    text <- str_replace_all(string = text, pattern = '\\\\_', '_')
+    text <- str_replace_all(string = text, pattern = '\\\\\\$', '$')
+    text <- str_replace_all(string = text, pattern = '\\\\\\\\', '\\n')
+    text <- str_replace_all(string = text, pattern = '\\\\cite[pt]\\*?\\{(.*?)\\}', replacement = '[@\\1]') 
+    if(save)  writeLines(text, paste0(input, ".Rmd"), sep = '')
 }
